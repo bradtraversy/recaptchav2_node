00fa611777808bcacf25a505185a6f3ba8b7f9ea
Brad Traversy <me@bradtraversy.com>
2017-09-23 08:16:35 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# reCAPTCHA v2 Node.js 
+
+A Node.js client-server implementation of Google's reCAPTCHA v2
+
+### Version
+1.0.0
+
+## Usage
+
+
+### Installation
+
+Install the dependencies
+
+```sh
+$ npm install
+```
+
+### Run
+To serve in the browser
+```sh
+$ npm start
+```
---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/subscribe', (req, res) => {
+  if(
+    req.body.captcha === undefined ||
+    req.body.captcha === '' ||
+    req.body.captcha === null
+  ){
+    return res.json({"success": false, "msg":"Please select captcha"});
+  }
+
+  // Secret Key
+  const secretKey = '6LdpvDEUAAAAAHszsgB_nnal29BIKDsxwAqEbZzU';
+
+  // Verify URL
+  const verifyUrl = `https://google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${req.body.captcha}&remoteip=${req.connection.remoteAddress}`;
+
+  // Make Request To VerifyURL
+  request(verifyUrl, (err, response, body) => {
+    body = JSON.parse(body);
+    console.log(body);
+
+    // If Not Successful
+    if(body.success !== undefined && !body.success){
+      return res.json({"success": false, "msg":"Failed captcha verification"});
+    }
+
+    //If Successful
+    return res.json({"success": true, "msg":"Captcha passed"});
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
